70e7c3f01d6d79ca2bb92b336b2bb4e07be249c1
Tejas Falke <tejasdf.77@gmail.com>
2023-03-24 17:50:12 +0530

    chat gpt question

---
+++ 1_python_basic/7_ChatGPT_problem.py
@@ -0,0 +1,7 @@
+list1 = [1,2,3,4,5,6,7,8,9,10]
+even_sum = 0
+
+for i in list1:
+    if i%2==0:
+        even_sum = even_sum+i
+print(even_sum)
---
+++ 1_python_basic/8_ChatGPT_problem.py
@@ -0,0 +1,8 @@
+# input :- n = 5
+# output :- 12345
+
+n = 5
+new_int = 0
+
+for i in range(1, n+1):
+    print(str(i),end="")
